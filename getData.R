84fa367630ebea2e28d9496407d73f262b6b94b7
sas6523 <scott.andrew.smith@gmail.com>
2015-03-19 11:39:07 -0400

    condensed code into loops

--- getData.R
+++ getData.R
@@ -30,7 +30,7 @@ files <- list.files(td, pattern = ".csv")
 names <- gsub(pattern='\\.csv$', '', files, ignore.case=T)
 
 setwd(td)
-# Load the files we just unziped into dataframes in R, then subset the df by state=37
+# Load the files we just unziped into dataframes in R,
 for( i in 1:length(names)){
         a <- paste(names[i], ' <- read.csv( file=\'', files[i], '\', header=TRUE, sep=\',\')', sep='')
         # print( a )
@@ -43,13 +43,6 @@ for (i in 1:length(names)){
         eval(parse( text=b ))
 }
 
-# Create empty df's to later populate
-merged.w1 <- data.frame()
-merged.w2 <- data.frame()
-merged.w3 <- data.frame()
-merged.w4 <- data.frame()
-merged.w5 <- data.frame()
-merged.w6 <- data.frame()
 
 # This function merges the df's passed to it based on the ID_CODE variable
 mergeDf <- function(catch,trip, i){
@@ -58,161 +51,64 @@ mergeDf <- function(catch,trip, i){
         return(eval(parse( text=b )))
 }
 
+# create dataframes and populate them by passing to mergedf. Then arrange them by id_code. For each wave.
+for (i in 1:6){
+        d <- paste0("merged.w",i," <- data.frame()")
+        a <- paste0("merged.w",i, " <- mergeDf(paste0('catch_', year, '",i,"'), paste0('trip_', year, '",i,"'), ",i,")")
+        b <- paste0("merged.w",i,"$common <- factor(merged.w",i,"$common)")
+        c <- paste0("merged.w",i, " <- arrange(merged.w",i,", desc(ID_CODE))")
+        eval(parse( text=d ))
+        eval(parse( text=a ))
+        eval(parse( text=b ))
+        eval(parse( text=c ))        
+}
 
-# TODO: Make th next 3 blocks into one for loop!
-# Merge catch and trip datasets by ID_CODE
-merged.w1 <- mergeDf(paste0('catch_', year, '1'), paste0('trip_', year, '1'), 1)
-merged.w2 <- mergeDf(paste0('catch_', year, '2'), paste0('trip_', year, '2'), 2)
-merged.w3 <- mergeDf(paste0('catch_', year, '3'), paste0('trip_', year, '3'), 3)
-merged.w4 <- mergeDf(paste0('catch_', year, '4'), paste0('trip_', year, '4'), 4)
-merged.w5 <- mergeDf(paste0('catch_', year, '5'), paste0('trip_', year, '5'), 5)
-merged.w6 <- mergeDf(paste0('catch_', year, '6'), paste0('trip_', year, '6'), 6)
 
-# Redefine the common names as factors. This will drop unused levels.
-merged.w1$common <- factor(merged.w1$common)
-merged.w2$common <- factor(merged.w2$common)
-merged.w3$common <- factor(merged.w3$common)
-merged.w4$common <- factor(merged.w4$common)
-merged.w5$common <- factor(merged.w5$common)
-merged.w6$common <- factor(merged.w6$common)
 
-# arrange by ID_CODE
-merged.w1 <- arrange(merged.w1, desc(ID_CODE))
-merged.w2 <- arrange(merged.w2, desc(ID_CODE))
-merged.w3 <- arrange(merged.w3, desc(ID_CODE))
-merged.w4 <- arrange(merged.w4, desc(ID_CODE))
-merged.w5 <- arrange(merged.w5, desc(ID_CODE))
-merged.w6 <- arrange(merged.w6, desc(ID_CODE))
-
-# create vectors of county codes to subset by
+# create vectors of county codes to subset by. These are the arbitrary divisions of N,S,Central in nc.
 south <- c(19, 95, 129, 133, 141, 147)
 north <- c(15, 29, 41, 53, 55, 139, 143, 177, 187)
 central <- c(13, 31, 49, 137)
 
-###############################################
-# The Following needs to be rewritten in functions
-###############################################
+
+for (i in 1:6){
+        #subset by county code into north and south NC
+        a <- paste0("merged.w",i,".south <- merged.w",i,"[merged.w",i,"$CNTY %in% south,]")
+        b <- paste0("merged.w",i,".north <- merged.w",i,"[merged.w",i,"$CNTY %in% north,]")
+        c <- paste0("merged.w",i,".central <- merged.w",i,"[merged.w",i,"$CNTY %in% central,]")
+        # Parse and eval all the strings
+        eval(parse( text=a ))
+        eval(parse( text=b ))
+        eval(parse( text=c ))
+        
+        # Redefine the common names as factors. This will drop unused levels.
+        d <- paste0("merged.w",i,".south$common <- factor(merged.w",i,".south$common)")
+        e <- paste0("merged.w",i,".north$common <- factor(merged.w",i,".north$common)")
+        f <- paste0("merged.w",i,".central$common <- factor(merged.w",i,".central$common)")
+        eval(parse( text=d ))
+        eval(parse( text=e ))
+        eval(parse( text=f ))
+        
+        # create a vector of fish names caught from each area
+        # I used m here since I couldnt reuse i
+        g <- paste0("unique.w",i,".south <- levels(merged.w",i,".south$common)")
+        h <- paste0("unique.w",i,".north <- levels(merged.w",i,".north$common)")
+        m <- paste0("unique.w",i,".central <- levels(merged.w",i,".central$common)")
+        eval(parse( text=g ))
+        eval(parse( text=h ))
+        eval(parse( text=m ))
+        
+        # Create a vector of the number of unique fish species caught
+        j <- paste0("num.caught.south.w",i," <- length(unique.w",i,".south)")
+        k <- paste0("num.caught.north.w",i," <- length(unique.w",i,".north)")
+        l <- paste0("num.caught.central.w",i," <- length(unique.w",i,".central)")
+        eval(parse( text=j ))
+        eval(parse( text=k ))
+        eval(parse( text=l ))
+
+}
 
 
-#subset by county code into north and south NC
-merged.w1.south <- merged.w1[merged.w1$CNTY %in% south,]
-merged.w1.north <- merged.w1[merged.w1$CNTY %in% north,]
-merged.w1.central <- merged.w1[merged.w1$CNTY %in% central,]
-
-# Redefine the common names as factors. This will drop unused levels.
-merged.w1.south$common <- factor(merged.w1.south$common)
-merged.w1.north$common <- factor(merged.w1.north$common)
-merged.w1.central$common <- factor(merged.w1.central$common)
-
-# create a vector of fish names caught from each area
-unique.w1.south <- levels(merged.w1.south$common)
-unique.w1.north <- levels(merged.w1.north$common)
-unique.w1.central <- levels(merged.w1.central$common)
-
-# Create a vector of the number of unique fish species caught
-num.caught.south.w1 <- length(unique.w1.south)
-num.caught.north.w1 <- length(unique.w1.north)
-num.caught.central.w1 <- length(unique.w1.central)
-
-#subset by county code into north and south NC
-merged.w2.south <- merged.w2[merged.w2$CNTY %in% south,]
-merged.w2.north <- merged.w2[merged.w2$CNTY %in% north,]
-merged.w2.central <- merged.w2[merged.w2$CNTY %in% central,]
-
-# Redefine the common names as factors. This will drop unused levels.
-merged.w2.south$common <- factor(merged.w2.south$common)
-merged.w2.north$common <- factor(merged.w2.north$common)
-merged.w2.central$common <- factor(merged.w2.central$common)
-
-# create a vector of fish names caught from each area
-unique.w2.south <- levels(merged.w2.south$common)
-unique.w2.north <- levels(merged.w2.north$common)
-unique.w2.central <- levels(merged.w2.central$common)
-
-# Create a vector of the number of unique fish species caught
-num.caught.south.w2 <- length(unique.w2.south)
-num.caught.north.w2 <- length(unique.w2.north)
-num.caught.central.w2 <- length(unique.w2.central)
-
-#subset by county code into north and south NC
-merged.w3.south <- merged.w3[merged.w3$CNTY %in% south,]
-merged.w3.north <- merged.w3[merged.w3$CNTY %in% north,]
-merged.w3.central <- merged.w3[merged.w3$CNTY %in% central,]
-
-# Redefine the common names as factors. This will drop unused levels.
-merged.w3.south$common <- factor(merged.w3.south$common)
-merged.w3.north$common <- factor(merged.w3.north$common)
-merged.w3.central$common <- factor(merged.w3.central$common)
-
-# create a vector of fish names caught from each area
-unique.w3.south <- levels(merged.w3.south$common)
-unique.w3.north <- levels(merged.w3.north$common)
-unique.w3.central <- levels(merged.w3.central$common)
-
-# Create a vector of the number of unique fish species caught
-num.caught.south.w3 <- length(unique.w3.south)
-num.caught.north.w3 <- length(unique.w3.north)
-num.caught.central.w3 <- length(unique.w3.central)
-
-#subset by county code into north and south NC
-merged.w4.south <- merged.w4[merged.w4$CNTY %in% south,]
-merged.w4.north <- merged.w4[merged.w4$CNTY %in% north,]
-merged.w4.central <- merged.w4[merged.w4$CNTY %in% central,]
-
-# Redefine the common names as factors. This will drop unused levels.
-merged.w4.south$common <- factor(merged.w4.south$common)
-merged.w4.north$common <- factor(merged.w4.north$common)
-merged.w4.central$common <- factor(merged.w4.central$common)
-
-# create a vector of fish names caught from each area
-unique.w4.south <- levels(merged.w4.south$common)
-unique.w4.north <- levels(merged.w4.north$common)
-unique.w4.central <- levels(merged.w4.central$common)
-
-# Create a vector of the number of unique fish species caught
-num.caught.south.w4 <- length(unique.w4.south)
-num.caught.north.w4 <- length(unique.w4.north)
-num.caught.central.w4 <- length(unique.w4.central)
-
-#subset by county code into north and south NC
-merged.w5.south <- merged.w5[merged.w5$CNTY %in% south,]
-merged.w5.north <- merged.w5[merged.w5$CNTY %in% north,]
-merged.w5.central <- merged.w5[merged.w5$CNTY %in% central,]
-
-# Redefine the common names as factors. This will drop unused levels.
-merged.w5.south$common <- factor(merged.w5.south$common)
-merged.w5.north$common <- factor(merged.w5.north$common)
-merged.w5.central$common <- factor(merged.w5.central$common)
-
-# create a vector of fish names caught from each area
-unique.w5.south <- levels(merged.w5.south$common)
-unique.w5.north <- levels(merged.w5.north$common)
-unique.w5.central <- levels(merged.w5.central$common)
-
-# Create a vector of the number of unique fish species caught
-num.caught.south.w5 <- length(unique.w5.south)
-num.caught.north.w5 <- length(unique.w5.north)
-num.caught.central.w5 <- length(unique.w5.central)
-
-#subset by county code into north and south NC
-merged.w6.south <- merged.w6[merged.w6$CNTY %in% south,]
-merged.w6.north <- merged.w6[merged.w6$CNTY %in% north,]
-merged.w6.central <- merged.w6[merged.w6$CNTY %in% central,]
-
-# Redefine the common names as factors. This will drop unused levels.
-merged.w6.south$common <- factor(merged.w6.south$common)
-merged.w6.north$common <- factor(merged.w6.north$common)
-merged.w6.central$common <- factor(merged.w6.central$common)
-
-# create a vector of fish names caught from each area
-unique.w6.south <- levels(merged.w6.south$common)
-unique.w6.north <- levels(merged.w6.north$common)
-unique.w6.central <- levels(merged.w6.central$common)
-
-# Create a vector of the number of unique fish species caught
-num.caught.south.w6 <- length(unique.w6.south)
-num.caught.north.w6 <- length(unique.w6.north)
-num.caught.central.w6 <- length(unique.w6.central)
 
 
 
